e59343c6d41cd4f75dc146ce34fe0eb67c0b978b
Asiha Braxton-Garvin <asiha14@gmail.com>
2020-08-12 19:10:51 -0400

    Question 2 Complete

--- EmployeeSQL/sql_challenge.sql
+++ EmployeeSQL/sql_challenge.sql
@@ -57,12 +57,13 @@ drop table dept_manager;
 select*from dept_manager;
 
 --List the following details of each employee: employee number, last name, first name, sex, and salary.
-SELECT e.emp_no,
-	e.first_name,
-	e.last_name,
-	e.sex,
-	s.salary
+SELECT e.emp_no, 	e.first_name, 	e.last_name, 	e.sex, 	s.salary
 FROM employees e
 JOIN salaries s
 ON e.emp_no = s.emp_no;
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date FROM employees
+where hire_date between '1986/01/01' and '1986/12/31';
+
 
